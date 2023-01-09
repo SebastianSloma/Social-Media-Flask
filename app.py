86a80ad19623c1ba56e7ae6112617dfda0ef313a
Sebastian <sebastiansloma@gmail.com>
2023-01-09 14:08:59 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,16 @@
+# import library
+
+from flask import Flask, render_template, redirect, request, url_for, flash, g, session
+import sqlite3
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
