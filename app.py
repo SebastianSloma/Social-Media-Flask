ba64ff6519e12bf3c3f19d3e6b0bf03ef32b7aef
Sebastian <sebastiansloma@gmail.com>
2023-01-11 20:22:33 +0100

    add function register and login

--- app.py
+++ app.py
@@ -1,23 +1,22 @@
 # import library
 
-from flask import Flask, render_template, redirect, request, url_for, flash, g, session
-import sqlite3
-from datetime import date
-import random
-import string
-import hashlib
-import binascii
+from flask import Flask, render_template, redirect, url_for, flash, request, session, logging
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
 
-
-app_info = {
-    'db_file': 'D:/nauka/Python/python_github/Social media - Flask/data/social.db'
-}
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'VeryWiredK3y!'
 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'socialmedia'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
+mysql = MySQL(app)
 
 
 @app.route('/')
@@ -25,15 +24,7 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/login')
-def login():           
-    return render_template('login.html')
-    
 
-
-@app.route('/registration')
-def registration():
-    return render_template('registration.html')
 
 
 @app.route('/about')
@@ -44,6 +35,70 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+
+class RegisterForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email', [validators.Length(min=6, max=50)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Password do not match')
+    ])
+    confirm = PasswordField('Confirm Password')
+
+
+@app.route('/registration', methods=['GET', 'POST'])
+def registration():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s,%s,%s,%s)",
+                    (name, email, username, password))
+        mysql.connection.commit()
+        cur.close()
+
+        flash('You are now registered and can log in', 'success')
+        return redirect(url_for('login'))
+
+    return render_template('registration.html', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password_candidate = request.form['password']
+
+        cur = mysql.connection.cursor()
+
+        result = cur.execute("SELECT * FROM users WHERE username = %s", [username])
+
+        if result > 0:
+            data = cur.fetchone()
+            password = data['password']
+
+            if sha256_crypt.verify(password_candidate, password):
+                session['logged_in'] = True
+                session['username'] = username
+
+                flash('You are now logged in', 'success')
+                return redirect(url_for('index'))
+            else:
+                error = 'Invalid login'
+                return render_template('login.html', error=error)
+            cur.close()
+        else:
+            error = 'Username not found'
+            return render_template('login.html', error=error)
+
+    return render_template('login.html')
+
 
 
 if __name__ == '__main__':
