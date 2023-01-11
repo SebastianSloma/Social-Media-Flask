060e03d5250ada26ca4dba33e108177f7e58e61b
Sebastian <sebastiansloma@gmail.com>
2023-01-11 09:34:50 +0100

    create contact page

--- app.py
+++ app.py
@@ -26,6 +26,10 @@ def registration():
 def about():
     return render_template('about.html')
 
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
